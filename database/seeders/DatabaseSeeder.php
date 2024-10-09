e4496e90cef1a7865bfb1b6ed870fae82ff5bd68
Leonardo-Veque <leonardo5veque@gmail.com>
2024-10-08 22:30:06 -0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            "name"          => "admin",
+            "password"      => Hash::make("123"),
+            "email"         => "admin@admin.com",
+        ]);
+    }
+}
